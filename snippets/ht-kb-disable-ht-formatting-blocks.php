e52ef906abd4956bc35937c0beb55daaacfd12fb
Richard Long <richard@local.dev>
2024-07-22 11:35:27 +0100

    refactoring disable formatting blocks snippet example

--- snippets/ht-kb-disable-ht-formatting-blocks.php
+++ snippets/ht-kb-disable-ht-formatting-blocks.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Disable the code formatting blocks module in the HeroThemes Knowledge Base
+* Disable the HeroThemes formatting blocks module in the HeroThemes Knowledge Base
 * Code snippet by @HeroThemes (https://herothemes.com) - Knowledge Base Themes and Plugins for WordPress
 * HeroThemes - Happier Customers, Fewer Support Tickets
 */
